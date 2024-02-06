feea6396d82adcb5230b08166713497bfa29f6eb
Rusty <proga100@gmail.com>
2024-02-06 16:23:12 +0500

    dd

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,11 @@
 	"use strict";
 
 	jQuery(document).ready(function ($) {
-
+		$('#menu_toggle').on('click', function () {
+			$(this).toggleClass('open');
+			$('.mobile_header .top_nav_mobile').slideToggle(300);
+			return false;
+		});
 
 	});
 
